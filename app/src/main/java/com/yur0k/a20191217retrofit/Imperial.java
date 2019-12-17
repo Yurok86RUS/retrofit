579d1480e6fd49ad5f01b6ca55c15b60213d5fb2
Yurok <yurok.kolesnikov@gmail.com>
2019-12-17 21:25:25 +0500

    create model & retrofit

---
+++ app/src/main/java/com/yur0k/a20191217retrofit/Imperial.java
@@ -0,0 +1,42 @@
+package com.yur0k.a20191217retrofit;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Imperial {
+
+    @SerializedName("Value")
+    @Expose
+    private Integer value;
+    @SerializedName("Unit")
+    @Expose
+    private String unit;
+    @SerializedName("UnitType")
+    @Expose
+    private Integer unitType;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getUnitType() {
+        return unitType;
+    }
+
+    public void setUnitType(Integer unitType) {
+        this.unitType = unitType;
+    }
+
+}
