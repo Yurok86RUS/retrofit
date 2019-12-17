3cabebaff9399b9b1b86df87baded2549c0f523e
Yurok <yurok.kolesnikov@gmail.com>
2019-12-17 20:17:24 +0500

    create project

---
+++ app/src/main/java/com/yur0k/a20191217retrofit/MainActivity.java
@@ -0,0 +1,14 @@
+package com.yur0k.a20191217retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
