f4d117038fb2885e8b58c97b4d7b3c743217ca26
Yurok <yurok.kolesnikov@gmail.com>
2019-12-18 22:33:31 +0500

    не идет дальше Response

--- app/src/main/java/com/yur0k/a20191217retrofit/MainActivity.java
+++ app/src/main/java/com/yur0k/a20191217retrofit/MainActivity.java
@@ -2,13 +2,17 @@ package com.yur0k.a20191217retrofit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
+import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -19,22 +23,32 @@ public class MainActivity extends AppCompatActivity {
     public static final String APPI_KEY = "dNCJ5Bk0LjZE75BNfUFBgAdOFj6I7bjI";
     private static final String TAG = "Log" ;
     public AccuWeather weatherApi;
+//    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textViewTemp = findViewById(R.id.textViewTemp);
+        final TextView textViewTemp = findViewById(R.id.textViewTemp);
 
         Log.e(TAG,"создаем активити");
 
         try {
             Log.i(TAG, "сейчас будем вызывать getWeather");
+//
+//            new Thread(new Runnable() {
+//                @Override
+//                public void run() {
 
-            WeatherModel model = getWeather(294021);
+                    WeatherModel model = null;
+                    try {
+                        model = getWeather(294021);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
 
-            Log.i(TAG,"model.getLink" + model.getLink());
+                    Log.i(TAG,"model.getLink" + model.getLink());
 
             if (model == null) {
                 Log.i(TAG, "model = null");
@@ -43,14 +57,19 @@ public class MainActivity extends AppCompatActivity {
             Log.i(TAG, "model != null получаем значение");
 
             textViewTemp.setText(Double.toString(model.getTemperature().getMetric().getValue()));
+//                }
+//            }).start();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+
     }
 
-    private WeatherModel getWeather (Integer cityName) throws Exception {
+
+    public WeatherModel getWeather (Integer cityName) throws Exception {
+
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).
                 addConverterFactory(GsonConverterFactory.create()).build();
 
@@ -58,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
         weatherApi = retrofit.create(AccuWeather.class);
 
-        Log.i(TAG, "weatherApi " + weatherApi.toString());
+        //Log.i(TAG, "weatherApi " + weatherApi.toString());
 
         Call<WeatherModel> call = weatherApi.getWeather(cityName,APPI_KEY);
 
@@ -67,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         Response<WeatherModel> response = call.execute();
 
         Log.i(TAG,"call.execute() = " + call.execute().toString());
-        Log.i(TAG,"response " + response);
+        Log.i(TAG,"response " + response.body());
 
         if (response.isSuccessful()) return response.body();
         else
